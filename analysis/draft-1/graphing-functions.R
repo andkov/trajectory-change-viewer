10bc1688fbce4500d5fc1d901895a3efb8b18233
Andriy V. Koval <koval.andrey@gmail.com>
2022-01-18 18:30:05 -0700

    Update graphing-functions.R

--- analysis/draft-1/graphing-functions.R
+++ analysis/draft-1/graphing-functions.R
@@ -44,27 +44,30 @@ percent_var_i    = "gender" # "percent from" will represent share from this cate
 # d0
 prep_data_trajectory <- function(
   d
-  ,outcome_var  # = "employed" # variable used for binning into age groups (default = years of age)
-  ,time_var         # = "year_fiscal"
+  # mandatory arguments
+  ,outcome_var          # = "employed" # variable used for binning into age groups (default = years of age)
+  ,time_var             # = "year_fiscal"
+  ,count_var            # = "id"
+  # optional arguments
   ,color_var     = NULL # = "gender"
   ,facet_row_var = NULL # = "race"
   ,facet_col_var = NULL # = "age"
   ,total_cat_var = NULL # = facet_row_var # new group "Total" will be inserted into this variable, summing across others
   ,percent_var   = NULL # = color_var # "percent from" will represent share from this category
   # prep level specific defaults
-  ,count_var      # = "id"
   # ,forecast_var = "cell_count"
 ){
+  # browser()
   # d <- ds1
-  # time_var          = "year_fiscal"
+  # outcome_var    = "employed" # variable used for binning into age groups (default = years of age)
+  # time_var       = "year_fiscal"
+  # count_var      = "id" # unique row ids used to compute `cell_count`
   # color_var      = "gender"
   # facet_row_var  = "age"              # rows
   # facet_col_var  = "race" # columns
   # total_cat_var  =  NULL # group "Total" added, summing across others
   # percent_var    = "gender" # "percent from ____" will represent share from this category
   # # prep level specific defaults
-  # count_var       = "id" # unique row ids used to compute `cell_count`
-  # outcome_var     = "employed" # variable used for binning into age groups (default = years of age)
   #
   # browser()
   (row_name           <- c(color_var, facet_row_var, facet_col_var) %>% unique())
@@ -72,9 +75,9 @@ prep_data_trajectory <- function(
   (total_group_vars   <- setdiff(most_granular_vars, total_cat_var) %>% unique())
   (percent_from_vars  <- setdiff(most_granular_vars, percent_var)   %>% unique())
   
-  get_custom_summary <- function(d){
+  get_custom_summary <- function(xd){
     # in current form, functions supports BINARY and NUMERIC outcomes
-    d %>%
+    xd %>%
       mutate(across(outcome_var %>% all_of(), as.numeric)) %>% 
       summarize(
         cell_count      = n_distinct(!!rlang::sym(count_var))
@@ -85,6 +88,7 @@ prep_data_trajectory <- function(
       ungroup() %>%
       mutate(across(where(is.factor), fct_drop)) # only non-empty levels
   }
+  # browser()
   
   #### STEP 1 - row counts at most granular level
   d1 <-
@@ -92,7 +96,7 @@ prep_data_trajectory <- function(
     group_by_at(vars(all_of(  most_granular_vars  ))) %>%
     get_custom_summary()
 
-  #### STEP 2  - if Total category is requested, it will be added as more rows
+  #### STEP 12a  - if Total category is requested, it will be added as more rows
   if(!is.null(total_cat_var)){
     d2 <-
       d %>%
@@ -102,10 +106,10 @@ prep_data_trajectory <- function(
         !!rlang::sym(total_cat_var) := "Total"
       )
     d12 <- dplyr::bind_rows(d1, d2) # adding new rows
-  } else {
+  }else{
     d12 <- d1 # if Total is not requested, we leave d1 as is
   }
-  #### STEP 3 - Percent computed
+  #### STEP 3 - Compute percent
   d3 <- 
     d12 %>%
     group_by_at(vars(all_of(  percent_from_vars  ))
@@ -115,19 +119,19 @@ prep_data_trajectory <- function(
       # ,cell_pct = cell_prop %>% scales::percent(accuracy = 1)
     ) %>%
     ungroup()
-  #### STEP 4 - 
+  #### STEP 12b - Tweak factors if adding Totals messed it up
   if(!is.null(total_cat_var)){
-    d4 <- 
+    d3 <-  # must overwrite because Total is optional
       d3 %>%
       mutate(
         !!rlang::sym(total_cat_var) := as_factor(!!rlang::sym(total_cat_var)) %>%
           fct_relevel("Total", after = Inf)
       )
   }
-  d_out <- d4
+  d_out <- d3
   # add forecast (optional) - to be developed
-  #   d5 <-
-  #     d4 %>%
+  #   d4 <-
+  #     d3 %>%
   #     add_prediction_row(
   #       outcome     = forecast_var
   #       ,timevar    = time_var
@@ -135,8 +139,8 @@ prep_data_trajectory <- function(
   #       ,row_name   = row_name
   #       ,latest_year_K = 2020
   #     )
-  # d5
-  # d_out <- d5
+  # d4
+  # d_out <- d4
   
   ls_out <- list(
     # "data" = d4
@@ -187,3 +191,68 @@ prep_data_trajectory <- function(
 
 
 
+
+# ---- plot-define -----------------------
+# works with the product of `prep_data_trajectory()`
+plot_trajectory <- function(
+  d
+  ,y_var       = "cell_prop" # cell_count or cell_prop
+  ,facet       = "grid"
+  ,scale_mode  = "free_y"
+){
+  
+  time_sym      <- rlang::sym( l[["meta"]][["time_var"]]      )
+  y_sym         <- rlang::sym( y_var                          )
+  color_sym     <- rlang::sym( l[["meta"]][["color_var"]]     ) 
+  facet_row_sym <- rlang::sym( l[["meta"]][["facet_row_var"]] )
+  facet_col_sym <- rlang::sym( l[["meta"]][["facet_col_var"]] )
+  facet_vars    <- c( facet_row_sym, facet_col_sym            ) 
+  percent_var   <- l[["meta"]][["percent_var"]]
+  # browser()
+  
+  g <-
+    l$data %>%
+    {
+      ggplot(., aes(
+        x      = !!time_sym
+        ,y     = !!y_sym
+        ,color = !!color_sym
+        ,group = !!color_sym
+      ))+
+        geom_line()+
+        geom_point()+
+        labs(
+          title    = paste0("Id count")
+          ,caption = paste0(
+            "Sum all levels of (", toupper(percent_var),") to get 100%"
+          )
+        )
+    }
+  if(facet == "grid"){
+    g <- 
+      g +
+      facet_grid(
+        rows    = vars( !!facet_row_sym )
+        ,cols   = vars( !!facet_col_sym )
+        ,scales = scale_mode
+      )
+  }
+  if(facet == "wrap"){
+    g <- 
+      g +
+      facet_wrap(
+        facets  = vars( !!facet_vars )
+        ,ncol   = l$data %>% pull( !!facet_col_sym ) %>% unique() %>% length()
+        ,scales = scale_mode
+      )
+  }
+  return(g)
+}
+
+#
+# ldp %>%
+#   plot_trajectory(
+#      y_var       = "cell_prop"
+#     ,facet       = "grid" # grid, wrap
+#     ,scale_mode = "free_y" # free , free_x , free_y, fixed
+#    )
