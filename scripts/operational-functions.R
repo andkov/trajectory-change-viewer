63f6d4e6ed1801694a0bc4a87673e44763e7d0ed
Andriy V. Koval <koval.andrey@gmail.com>
2022-01-20 08:53:42 -0700

    Update operational-functions.R

--- scripts/operational-functions.R
+++ scripts/operational-functions.R
@@ -1,28 +1,9 @@
 # Functions loaded by SOME scripts in the project
 
+# ----- compute ----------------------------------------------------------------
 compute_fiscal_year <- function(x)(as.integer(zoo::as.yearmon(x) - 3/12 ))
 
-
-# Define bottom and right limits when import Excel sheet
-trim_sheet_input <- function(
-  d
-  , skip_n_rows      = 0
-  , total_n_rows     = nrow(d)
-  , column_positions = 1:ncol(d)
-
-){
-  # browser()
-  # d <- dto$EmpButLost
-  # skip_n_rows  <- 4
-  # total_n_rows <- 35
-  # column_positions <- c(1,2,3)
-  # nrow(d)
-  d_out <- d %>%
-    slice( (skip_n_rows+1):total_n_rows ) %>%
-    select(all_of(column_positions))
-  return(d_out)
-}
-
+# ----- sampling ---------------------------------------------------------------
 
 draw_random_id <- function(d, idvar="person_oid", n = 1){
   all_unique_ids <- d %>%
@@ -58,3 +39,24 @@ get_sample <- function(
 # How to use
 # a_sample <- ds2 %>% get_sample_uniques(sample_size =10, "person_oid")
 # a_sample <- ds2 %>% get_sample_uniques(10)
+
+# ----- data-input -------------------------------------------------------------
+# Define bottom and right limits when import Excel sheet
+trim_sheet_input <- function(
+  d
+  , skip_n_rows      = 0
+  , total_n_rows     = nrow(d)
+  , column_positions = 1:ncol(d)
+  
+){
+  # browser()
+  # d <- dto$EmpButLost
+  # skip_n_rows  <- 4
+  # total_n_rows <- 35
+  # column_positions <- c(1,2,3)
+  # nrow(d)
+  d_out <- d %>%
+    slice( (skip_n_rows+1):total_n_rows ) %>%
+    select(all_of(column_positions))
+  return(d_out)
+}
